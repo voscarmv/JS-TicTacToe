5d246e61f06154a9e10e4701252c1e0a85a9f27c
Oscar Mier <voscarmv@gmail.com>
2020-07-14 17:00:43 -0500

    Created player factory.

---
+++ script/script.js
@@ -0,0 +1,14 @@
+const Player = (name, symbol) => {
+  let score = 0;
+  const getName = () => name;
+  const getSymbol = () => symbol;
+  const getScore = () => score;
+  const win = () => {
+    score += 1;
+    return score;
+  };
+  return {getName, getSymbol, getScore, win}
+};
+
+const player1 = Player("Oscar", "O");
+const player2 = Player("Sergio", "S");
