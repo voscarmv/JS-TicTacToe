8bbb7bc2177b8338bbaf2ab5eb7fed1c678fc33f
sergiomauz <sergio.mauz88@gmail.com>
2020-07-15 15:15:04 -0500

    Added enabled and disabled gameboard

--- script/script.js
+++ script/script.js
@@ -23,6 +23,10 @@ const Gameboard = (() => {
     }
     return false;
   };
+  const draw = () => {
+    return !board.includes(' ');
+  };
+
   const print = () => {
     let divGameBoard = `    
       <table>
@@ -46,6 +50,7 @@ const Gameboard = (() => {
       </table>
     `;
 
+    document.getElementById('divHeaderGameBoard').innerHTML = "Let's play!";
     document.getElementById('divGameBoard').innerHTML = divGameBoard;
   };
   const reset = () => {
@@ -53,7 +58,7 @@ const Gameboard = (() => {
       board[i] = ' ';
     }
   }
-  return { move, print, empty, winner, reset }
+  return { move, print, empty, winner, reset, draw }
 })();
 
 const Player = (name, symbol) => {
@@ -73,6 +78,7 @@ const GameControl = (() => {
   let p1 = null;
   let p2 = null;
   let current_player = p1;
+  let enabled = true;
   let createPlayers = (p1_name, p2_name) => {
     p1 = Player(p1_name, 'X');
     p2 = Player(p2_name, 'O');
@@ -96,65 +102,78 @@ const GameControl = (() => {
       </div>
       <button id="start_game" type="submit" class="btn btn-primary">Start!</button>
     `;
+    document.getElementById('divHeaderControls').innerHTML = "Players' names";
     document.getElementById('divGameControls').innerHTML = signUp;
     document.getElementById('start_game').addEventListener('click', () => {
       p1_name = document.getElementById('player1').value;
       p2_name = document.getElementById('player2').value;
-      createPlayers(p1_name, p2_name);
-      current_player = p1;
-      document.getElementById('divBoardCard').className = 'card';
-      addEvents();
-      altScoreBoard();
+
+      document.getElementById('invalid-p1').className = 'alert alert-danger d-none';
+      document.getElementById('invalid-p2').className = 'alert alert-danger d-none';
+      if (p1_name.length > 0 && p2_name.length > 0) {
+        createPlayers(p1_name, p2_name);
+        current_player = p1;
+        document.getElementById('divBoardCard').className = 'card mr-3';
+        addEvents();
+        altScoreBoard();
+      }
+      else {
+        if (p1_name.length === 0) {
+          document.getElementById('invalid-p1').className = 'alert alert-danger';
+        }
+        if (p2_name.length === 0) {
+          document.getElementById('invalid-p2').className = 'alert alert-danger';
+        }
+      }
     });
   };
 
   const altScoreBoard = () => {
     let scoreBoard = `
-      <div>${p1.getName()} V.S. ${p2.getName()}</div>
-      <div>${p1.getName()}: ${p1.getScore()} victories.</div>
-      <div>${p2.getName()}: ${p2.getScore()} victories.</div>
+      <div class='mb-2 font-weight-bold'>${p1.getName()} V.S. ${p2.getName()}</div>
+      <div class='mb-2'>${p1.getName()}: ${p1.getScore()} victories.</div>
+      <div class='mb-3'>${p2.getName()}: ${p2.getScore()} victories.</div>
       <button id='btnResetBoard' class='btn btn-primary'>Reset the board</button>
     `;
+    document.getElementById('divHeaderControls').innerHTML = 'Game Score';
     document.getElementById('divGameControls').innerHTML = scoreBoard;
     document.getElementById('btnResetBoard').addEventListener('click', () => {
       board.reset();
       for (let i = 0; i < 9; i++) {
         document.getElementById(`cell_${i}`).innerHTML = ' ';
       }
+      enabled = true;
+      document.getElementById('divHeaderGameBoard').innerHTML = "Let's play!!";
+      document.getElementById('divHeaderGameBoard').className = 'card-header';
     });
   };
 
   const addEvents = () => {
     for (let i = 0; i < 9; i++) {
       document.getElementById(`cell_${i}`).addEventListener('click', () => {
-        if (board.empty(i)) {
+        if (board.empty(i) && enabled) {
           board.move(current_player.getSymbol(), i);
           document.getElementById(`cell_${i}`).innerHTML = current_player.getSymbol();
           let wins = board.winner(current_player.getSymbol());
           if (wins) {
             current_player.win();
             altScoreBoard();
-            console.log(`p1 score ${p1.getScore()}`);
-            console.log(`p2 score ${p2.getScore()}`);
-            console.log(`current_player score ${current_player.getScore()}`);
-
-            //document.getElementById('divGameControls').innerHTML = `${current_player.getName()} wins!`;
+            document.getElementById('divHeaderGameBoard').innerHTML = `${current_player.getName()} wins!`;
+            document.getElementById('divHeaderGameBoard').className = 'card-header bg-success';
+            enabled = false;
+          }
+          else if (board.draw()) {
+            document.getElementById('divHeaderGameBoard').innerHTML = "It's a draw!";
+            document.getElementById('divHeaderGameBoard').className = 'card-header bg-warning';
           }
           current_player === p1 ? current_player = p2 : current_player = p1;
         }
-
       });
     }
-  };
-
-  const gameLoop = () => {
-
   };
   return { initForm }
 })();
 
-const player1 = Player("Oscar", "O");
-const player2 = Player("Sergio", "S");
 
 Gameboard.print();
 GameControl.initForm();
