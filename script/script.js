bb5acf76309981ca63ed9551d5ffd7282efb80df
Oscar Mier <voscarmv@gmail.com>
2020-07-16 10:58:08 -0500

    Started separating game logic from DOM.

--- script/script.js
+++ script/script.js
@@ -1,3 +1,20 @@
+const UserInterfaceControl = (() => {
+  const printSignUp = () =>{
+    let signUp = document.getElementById('signup_template');
+    let clon = signUp.content.cloneNode(true);
+    document.getElementById('divGameControls').appendChild(clon);
+  };
+  const printBoard = (board) =>{
+    let gameBoard = document.getElementById('game_board');
+    for(let i = 0; i < 9; i += 1){
+      gameBoard.querySelectorAll(`#cell_${i}`).innerHTML = board[i];
+    }
+    let clon = gameBoard.content.cloneNode(true);
+    document.getElementById('divGameBoard').appendChild(clon);
+  };
+  return { printSignUp, printBoard }
+})();
+
 const Gameboard = (() => {
   const board = [
     ' ', ' ', ' ',
@@ -35,30 +52,32 @@ const Gameboard = (() => {
   const draw = () => !board.includes(' ');
 
   const print = () => {
-    const divGameBoard = `    
-      <table>
-        <tbody>
-          <tr>
-            <td class='border border-dark'><div id='cell_0' class='cell d-flex justify-content-center align-items-center'>${board[0]}</div></td>
-            <td class='border border-dark'><div id='cell_1' class='cell d-flex justify-content-center align-items-center'>${board[1]}</div></td>
-            <td class='border border-dark'><div id='cell_2' class='cell d-flex justify-content-center align-items-center'>${board[2]}</div></td>
-          </tr>
-          <tr>
-            <td class='border border-dark'><div id='cell_3' class='cell d-flex justify-content-center align-items-center'>${board[3]}</div></td>
-            <td class='border border-dark'><div id='cell_4' class='cell d-flex justify-content-center align-items-center'>${board[4]}</div></td>
-            <td class='border border-dark'><div id='cell_5' class='cell d-flex justify-content-center align-items-center'>${board[5]}</div></td>
-          </tr>
-          <tr>
-            <td class='border border-dark'><div id='cell_6' class='cell d-flex justify-content-center align-items-center'>${board[6]}</div></td>
-            <td class='border border-dark'><div id='cell_7' class='cell d-flex justify-content-center align-items-center'>${board[7]}</div></td>
-            <td class='border border-dark'><div id='cell_8' class='cell d-flex justify-content-center align-items-center'>${board[8]}</div></td>
-          </tr>          
-        </tbody>
-      </table>
-    `;
+    // const divGameBoard = `    
+    //   <table>
+    //     <tbody>
+    //       <tr>
+    //         <td class='border border-dark'><div id='cell_0' class='cell d-flex justify-content-center align-items-center'>${board[0]}</div></td>
+    //         <td class='border border-dark'><div id='cell_1' class='cell d-flex justify-content-center align-items-center'>${board[1]}</div></td>
+    //         <td class='border border-dark'><div id='cell_2' class='cell d-flex justify-content-center align-items-center'>${board[2]}</div></td>
+    //       </tr>
+    //       <tr>
+    //         <td class='border border-dark'><div id='cell_3' class='cell d-flex justify-content-center align-items-center'>${board[3]}</div></td>
+    //         <td class='border border-dark'><div id='cell_4' class='cell d-flex justify-content-center align-items-center'>${board[4]}</div></td>
+    //         <td class='border border-dark'><div id='cell_5' class='cell d-flex justify-content-center align-items-center'>${board[5]}</div></td>
+    //       </tr>
+    //       <tr>
+    //         <td class='border border-dark'><div id='cell_6' class='cell d-flex justify-content-center align-items-center'>${board[6]}</div></td>
+    //         <td class='border border-dark'><div id='cell_7' class='cell d-flex justify-content-center align-items-center'>${board[7]}</div></td>
+    //         <td class='border border-dark'><div id='cell_8' class='cell d-flex justify-content-center align-items-center'>${board[8]}</div></td>
+    //       </tr>          
+    //     </tbody>
+    //   </table>
+    // `;
+
+    UserInterfaceControl.printBoard(board);
 
     document.getElementById('divHeaderGameBoard').innerHTML = "Let's play!";
-    document.getElementById('divGameBoard').innerHTML = divGameBoard;
+    // document.getElementById('divGameBoard').innerHTML = divGameBoard;
   };
   const reset = () => {
     for (let i = 0; i < 9; i += 1) {
@@ -86,6 +105,7 @@ const Player = (name, symbol) => {
 
 const GameControl = (() => {
   const board = Gameboard;
+  const uicontrol = UserInterfaceControl;
   let p1 = null;
   let p2 = null;
   let currentPlayer = p1;
@@ -127,15 +147,15 @@ const GameControl = (() => {
         document.getElementById('divHeaderGameBoard').innerHTML = `${currentPlayer.getName()} wins!`;
         document.getElementById('divHeaderGameBoard').className = 'font-weight-bold text-white card-header bg-success';
         enabled = false;
-        if (firstPlayer === p1) { firstPlayer = p2; } else { firstPlayer = p1; }
+        if(firstPlayer === p1){ firstPlayer = p2; } else { firstPlayer = p1; }
         currentPlayer = firstPlayer;
       } else if (board.draw()) {
         document.getElementById('divHeaderGameBoard').innerHTML = "It's a draw!";
         document.getElementById('divHeaderGameBoard').className = 'font-weight-bold card-header bg-warning';
-        if (firstPlayer === p1) { firstPlayer = p2; } else { firstPlayer = p1; }
+        firstPlayer === p1 ? firstPlayer = p2 : firstPlayer = p1;
         currentPlayer = firstPlayer;
       }
-      if (currentPlayer === p1) { currentPlayer = p2; } else { currentPlayer = p1; }
+      currentPlayer === p1 ? currentPlayer = p2 : currentPlayer = p1;
     }
   };
 
@@ -148,26 +168,27 @@ const GameControl = (() => {
   };
 
   const initForm = () => {
-    let signUp = '';
-    signUp = `
-      <div class="form-group">
-      <label for="player1">Player 1</label>
-      <input type="text" class="form-control" id="player1" placeholder="Player 1" />
-        <div id="invalid-p1" class="alert alert-danger d-none" role="alert">
-          Player 1 name not valid.
-        </div>
-      </div>
-      <div class="form-group">
-      <label for="player2">Player 2</label>
-      <input type="text" class="form-control" id="player2" placeholder="Player 2" />
-        <div id="invalid-p2" class="alert alert-danger d-none" role="alert">
-          Player 2 name not valid.
-        </div>
-      </div>
-      <button id="start_game" type="submit" class="btn btn-primary">Start!</button>
-    `;
+    // let signUp = '';
+    // signUp = `
+    //   <div class="form-group">
+    //   <label for="player1">Player 1</label>
+    //   <input type="text" class="form-control" id="player1" placeholder="Player 1" />
+    //     <div id="invalid-p1" class="alert alert-danger d-none" role="alert">
+    //       Player 1 name not valid.
+    //     </div>
+    //   </div>
+    //   <div class="form-group">
+    //   <label for="player2">Player 2</label>
+    //   <input type="text" class="form-control" id="player2" placeholder="Player 2" />
+    //     <div id="invalid-p2" class="alert alert-danger d-none" role="alert">
+    //       Player 2 name not valid.
+    //     </div>
+    //   </div>
+    //   <button id="start_game" type="submit" class="btn btn-primary">Start!</button>
+    // `;
+    uicontrol.printSignUp();
     document.getElementById('divHeaderControls').innerHTML = "Players' names";
-    document.getElementById('divGameControls').innerHTML = signUp;
+    // document.getElementById('divGameControls').innerHTML = signUp;
     document.getElementById('start_game').addEventListener('click', () => {
       const p1Name = document.getElementById('player1').value;
       const p2Name = document.getElementById('player2').value;
@@ -193,6 +214,8 @@ const GameControl = (() => {
 
   return { initForm };
 })();
+
+
 
 
 Gameboard.print();
