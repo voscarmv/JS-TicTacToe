39675f7d322bdedaeae08a3daa2250e83e842bc8
Oscar Mier <voscarmv@gmail.com>
2020-07-16 14:32:20 -0500

    Finished separating all DOM manipulation inside UserInterfaceControl module.

--- script/script.js
+++ script/script.js
@@ -1,8 +1,29 @@
 const UserInterfaceControl = (() => {
-  const printSignUp = () => {
+  const printSignUp = (prepareBoard, makePlayers) => {
     let signUp = document.getElementById('signup_template');
     let clon = signUp.content.cloneNode(true);
     document.getElementById('divGameControls').appendChild(clon);
+
+    document.getElementById('divHeaderControls').innerHTML = "Players' names";
+    document.getElementById('start_game').addEventListener('click', () => {
+      const p1Name = document.getElementById('player1').value;
+      const p2Name = document.getElementById('player2').value;
+
+      document.getElementById('invalid-p1').className = 'alert alert-danger d-none';
+      document.getElementById('invalid-p2').className = 'alert alert-danger d-none';
+      if (p1Name.length > 0 && p2Name.length > 0) {
+        makePlayers(p1Name, p2Name);
+        prepareBoard();
+        document.getElementById('divBoardCard').className = 'card mr-3';
+      } else {
+        if (p1Name.length === 0) {
+          document.getElementById('invalid-p1').className = 'alert alert-danger';
+        }
+        if (p2Name.length === 0) {
+          document.getElementById('invalid-p2').className = 'alert alert-danger';
+        }
+      }
+    });
   };
   const printBoard = () => {
     let gameBoard = document.getElementById('game_board');
@@ -10,7 +31,7 @@ const UserInterfaceControl = (() => {
     document.getElementById('divGameBoard').appendChild(clon);
     document.getElementById('divHeaderGameBoard').innerHTML = "Let's play!";
   };
-  const printScoreBoard = (player1, player2) => {
+  const printScoreBoard = (player1, player2, enable) => {
     let score = document.getElementById('score_board');
     score.content.querySelector('#players_match').innerHTML = `${player1.getName()} V.S. ${player2.getName()}`;
     score.content.querySelector('#player1_score').innerHTML = `${player1.getName()}: ${player1.getScore()} victories.`;
@@ -25,11 +46,42 @@ const UserInterfaceControl = (() => {
       }
       document.getElementById('divHeaderGameBoard').innerHTML = "Let's play!!";
       document.getElementById('divHeaderGameBoard').className = 'card-header';
+      enable();
     });
     document.getElementById('divHeaderControls').innerHTML = 'Game Score';
   }
 
-  return { printSignUp, printBoard, printScoreBoard }
+  const printWinMsg = (player) => {
+    document.getElementById('divHeaderGameBoard').innerHTML = `${player.getName()} wins!`;
+    document.getElementById('divHeaderGameBoard').className = 'font-weight-bold text-white card-header bg-success';
+  };
+
+  const gameDraw = () => {
+    document.getElementById('divHeaderGameBoard').innerHTML = "It's a draw!";
+    document.getElementById('divHeaderGameBoard').className = 'font-weight-bold card-header bg-warning';
+  };
+
+  const setSymbolOnBoard = (index, player) => {
+    document.getElementById(`cell_${index}`).innerHTML = player.getSymbol();
+  };
+
+  const addBoardEvents = (clickMethod) => {
+    for (let i = 0; i < 9; i += 1) {
+      document.getElementById(`cell_${i}`).addEventListener('click', () => {
+        clickMethod(i);
+      });
+    }
+  };
+
+  return {
+    printSignUp,
+    printBoard,
+    printScoreBoard,
+    printWinMsg,
+    gameDraw,
+    setSymbolOnBoard,
+    addBoardEvents
+  }
 })();
 
 const Gameboard = (() => {
@@ -97,7 +149,6 @@ const Player = (name, symbol) => {
 
 const GameControl = (() => {
   const board = Gameboard;
-  const uicontrol = UserInterfaceControl;
   let p1 = null;
   let p2 = null;
   let currentPlayer = p1;
@@ -108,27 +159,28 @@ const GameControl = (() => {
     p2 = Player(p2Name, '🌘');
   };
 
-  const altScoreBoard = () => {
-    UserInterfaceControl.printScoreBoard(p1, p2);
+  const enableBoard = () => {
     enabled = true;
+  }
+
+  const altScoreBoard = () => {
+    UserInterfaceControl.printScoreBoard(p1, p2, enableBoard);
   };
 
   const clickEvent = (index) => {
     if (board.empty(index) && enabled) {
       board.move(currentPlayer.getSymbol(), index);
-      document.getElementById(`cell_${index}`).innerHTML = currentPlayer.getSymbol();
+      UserInterfaceControl.setSymbolOnBoard(index, currentPlayer);
       const wins = board.winner(currentPlayer.getSymbol());
       if (wins) {
         currentPlayer.win();
         altScoreBoard();
-        document.getElementById('divHeaderGameBoard').innerHTML = `${currentPlayer.getName()} wins!`;
-        document.getElementById('divHeaderGameBoard').className = 'font-weight-bold text-white card-header bg-success';
+        UserInterfaceControl.printWinMsg(currentPlayer);
         enabled = false;
         if (firstPlayer === p1) { firstPlayer = p2; } else { firstPlayer = p1; }
         currentPlayer = firstPlayer;
       } else if (board.draw()) {
-        document.getElementById('divHeaderGameBoard').innerHTML = "It's a draw!";
-        document.getElementById('divHeaderGameBoard').className = 'font-weight-bold card-header bg-warning';
+        UserInterfaceControl.gameDraw();
         firstPlayer === p1 ? firstPlayer = p2 : firstPlayer = p1;
         currentPlayer = firstPlayer;
       }
@@ -137,37 +189,17 @@ const GameControl = (() => {
   };
 
   const addEvents = () => {
-    for (let i = 0; i < 9; i += 1) {
-      document.getElementById(`cell_${i}`).addEventListener('click', () => {
-        clickEvent(i);
-      });
-    }
+    UserInterfaceControl.addBoardEvents(clickEvent)
+  };
+
+  const prepareBoard= () =>{
+    currentPlayer = p1;
+    addEvents();
+    altScoreBoard();
   };
 
   const initForm = () => {
-    uicontrol.printSignUp();
-    document.getElementById('divHeaderControls').innerHTML = "Players' names";
-    document.getElementById('start_game').addEventListener('click', () => {
-      const p1Name = document.getElementById('player1').value;
-      const p2Name = document.getElementById('player2').value;
-
-      document.getElementById('invalid-p1').className = 'alert alert-danger d-none';
-      document.getElementById('invalid-p2').className = 'alert alert-danger d-none';
-      if (p1Name.length > 0 && p2Name.length > 0) {
-        createPlayers(p1Name, p2Name);
-        currentPlayer = p1;
-        document.getElementById('divBoardCard').className = 'card mr-3';
-        addEvents();
-        altScoreBoard();
-      } else {
-        if (p1Name.length === 0) {
-          document.getElementById('invalid-p1').className = 'alert alert-danger';
-        }
-        if (p2Name.length === 0) {
-          document.getElementById('invalid-p2').className = 'alert alert-danger';
-        }
-      }
-    });
+    UserInterfaceControl.printSignUp(prepareBoard, createPlayers);
   };
 
   return { initForm };
